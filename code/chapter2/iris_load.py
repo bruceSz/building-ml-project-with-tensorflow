12ad444a4e89542137c8a05d21929fa66f84bcbb
bruceSz <zsyuyizhang@gmail.com>
2018-06-21 08:02:10 +0800

    iris loader code

--- code/chapter2/iris_load.py
+++ code/chapter2/iris_load.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
 sess = tf.Session()
 
-
+# combine multi filenames to one q.
 file_name_q = tf.train.string_input_producer(
-    tf.train.match_filenames_once(".")
+    tf.train.match_filenames_once("./data/iris.csv"),
+    shuffle=True
 )
+reader = tf.TextLineReader(skip_header_lines=1)
+k,v  = reader.read(file_name_q)
+record_d = [[0.],[0.],[0.],[0.],[""]]
+c1,c2,c3,c4,c5 = tf.decode_csv(v,
+                               record_defaults=record_d)
+fts = tf([c1,c3,c3,c4,c5])
+tf.global_variables_initializer().run(session=sess)
--- code/chapter2/load_dataset.py
+++ code/chapter2/load_dataset.py
@@ -1,13 +1,32 @@
 
 import sys
 from sklearn import datasets
+import numpy as np
+import pandas as pd
+
+def combineDataTarget(data,target,target_explain):
+    targe_new = np.expand_dims(target,1)
+
+    target_names = np.array([target_explain[i] for i in target])
+    target_names_new = np.expand_dims(target_names,1)
+    ret = np.concatenate((data,targe_new,target_names_new),1)
+    return ret
 
 
 class DataLoader(object):
 
     def load(self, name):
         if name == "iris":
-            return datasets.load_iris()
+            raw =  datasets.load_iris()
+            print raw['target_names']
+            c_data = combineDataTarget(raw['data']
+                                       ,raw['target']
+                                       ,raw['target_names'])
+            columns = raw['feature_names']
+            columns += ['flow_type_id']
+            columns += ['flower_type']
+            df = pd.DataFrame(c_data,columns=columns)
+            return df
         else:
             raise Exception("Unsupport dataset name: "+name)
 
@@ -18,10 +37,9 @@ class DataCSVSaver(object):
 def main(args):
     name = args
     dl = DataLoader()
-    da = dl.load("iris")
-    print da['target_names']
-    print da['feature_names']
-    print 
+    ds = DataCSVSaver()
+    df = dl.load("iris")
+    ds.save("./data/iris.csv",df)
 
 
 if __name__ == "__main__":
